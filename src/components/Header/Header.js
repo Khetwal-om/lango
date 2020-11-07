a768d63b36cd2b574920434e5a9ee52fdb4d8bce
khetwal-om <khetwalom@gmail.com>
2020-11-07 21:21:22 +0530

    Favicon updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import './Header.css'
+import Lango from '../../img/Lango.png'
 class Navbar extends Component {
   render() {
     return (
       <div className="background__navbar">
         <nav className="navbar container">
-          <NavLink  to="/" className="logo"><img src="https://firebasestorage.googleapis.com/v0/b/eat-me-608ee.appspot.com/o/storytelling.svg?alt=media&token=299bb6c0-c652-406a-bc50-26054c2d31cf" alt=""/></NavLink>
+          <NavLink  to="/" className="logo"><img src={Lango} alt=""/></NavLink>
           <ul className="navbar-menu">
             <li className="">
               <NavLink to="/shayna" className="">
